fa4cf7da48ec57d72e8435e8e46c7e3dfb5b47e6
74th <site@j74th.com>
2018-07-10 09:26:28 +0900

    update for python3

--- settingsjson/settingsjson.py
+++ settingsjson/settingsjson.py
@@ -1,9 +1,10 @@
 import os
 import json
 
-# search settings.json
+
 def get(filename=".settings.json", basepath=None):
-    if basepath == None :
+    """get and parse .settings.json(or setted filename) from current(or basepath) and parrent directories"""
+    if basepath == None:
         path = os.getcwd()
     else:
         path = os.path.abspath(basepath)
@@ -15,6 +16,9 @@ def get(filename=".settings.json", basepath=None):
             f.close()
             return d
         before_path = path
-        path = os.path.abspath(os.path.join(path,os.pardir))
-        if not os.path.exists(path) or path == before_path :
-            raise Exception("not found .setting.json")
+        path = os.path.abspath(os.path.join(path, os.pardir))
+        if not os.path.exists(path) or path == before_path:
+            if basepath == None:
+                raise Exception("not found {0}".format(filename))
+            else:
+                raise Exception("not found {0} in {1}".format(filename, basepath))
